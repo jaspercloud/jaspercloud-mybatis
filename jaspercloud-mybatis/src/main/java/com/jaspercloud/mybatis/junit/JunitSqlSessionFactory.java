3ed7bc09ad5d51f71a08fe37ebea990994809c7a
jaspercloud <276726581@qq.com>
2021-12-12 12:52:25 +0800

    junit setMapperLocations支持

--- jaspercloud-mybatis/src/main/java/com/jaspercloud/mybatis/junit/JunitSqlSessionFactory.java
+++ jaspercloud-mybatis/src/main/java/com/jaspercloud/mybatis/junit/JunitSqlSessionFactory.java
@@ -8,15 +8,22 @@ import com.baomidou.mybatisplus.core.injector.ISqlInjector;
 import com.baomidou.mybatisplus.core.mapper.Mapper;
 import com.baomidou.mybatisplus.core.toolkit.GlobalConfigUtils;
 import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import com.jaspercloud.mybatis.exception.ResourceException;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.transaction.TransactionFactory;
 import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 
 import javax.sql.DataSource;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class JunitSqlSessionFactory implements FactoryBean<SqlSessionFactory>, InitializingBean {
 
@@ -27,6 +34,7 @@ public class JunitSqlSessionFactory implements FactoryBean<SqlSessionFactory>, I
     private TransactionFactory transactionFactory;
     private SqlSessionFactory sqlSessionFactory;
     private List<Class<? extends Mapper>> mapperList = new ArrayList<>();
+    private List<String> mapperLocationList = new ArrayList<>();
 
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
@@ -52,6 +60,10 @@ public class JunitSqlSessionFactory implements FactoryBean<SqlSessionFactory>, I
         this.mapperList.add(clazz);
     }
 
+    public void addMapperLocation(String location) {
+        this.mapperLocationList.add(location);
+    }
+
     @Override
     public void afterPropertiesSet() throws Exception {
         GlobalConfig globalConfig = GlobalConfigUtils.defaults();
@@ -64,6 +76,17 @@ public class JunitSqlSessionFactory implements FactoryBean<SqlSessionFactory>, I
         factory.setConfiguration(null == configuration ? new MybatisConfiguration() : configuration);
         factory.setTransactionFactory(null == transactionFactory ? new JdbcTransactionFactory() : transactionFactory);
         factory.setGlobalConfig(globalConfig);
+        ResourcePatternResolver resourceResolver = new PathMatchingResourcePatternResolver();
+        factory.setMapperLocations(mapperLocationList.stream()
+                .map(mapperLocation -> {
+                    try {
+                        Resource[] mappers = resourceResolver.getResources(mapperLocation);
+                        return Arrays.asList(mappers);
+                    } catch (IOException e) {
+                        throw new ResourceException(e.getMessage(), e);
+                    }
+                }).flatMap(e -> e.stream())
+                .collect(Collectors.toList()).toArray(new Resource[0]));
         factory.afterPropertiesSet();
         sqlSessionFactory = factory.getObject();
         for (Class<? extends Mapper> mapper : mapperList) {
